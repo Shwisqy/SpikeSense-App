3cb143ce87be6949242f3a9957509f7090a1bbbd
Shwisqy <140091249+Shwisqy@users.noreply.github.com>
2026-02-24 12:49:23 -0500

    Delete native directory

--- native/windows/background/background.js
+++
@@ -1,9 +0,0 @@
-import { BackgroundController } from './background-controller.js';
-
-console.log('🚀 Starting Background Controller...');
-
-const backgroundController = new BackgroundController();
-
-backgroundController.run().catch(e => console.error(e));
-
-console.log('✅ Background Controller initialized');
